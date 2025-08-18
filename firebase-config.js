d17e22ea22e44bb927699ea617414d3c5fcb5279
Katsun1246 <96658284+Katsun1236@users.noreply.github.com>
2025-08-18 15:48:09 +0000

    test

---
+++ firebase-config.js
@@ -0,0 +1,24 @@
+// Importez les fonctions nécessaires depuis les SDK Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
+// TODO: Remplacez l'objet ci-dessous par la configuration de VOTRE projet Firebase
+// Vous trouverez ces informations dans les paramètres de votre projet sur la console Firebase.
+const firebaseConfig = {
+  apiKey: "VOTRE_API_KEY",
+  authDomain: "VOTRE_AUTH_DOMAIN",
+  projectId: "VOTRE_PROJECT_ID",
+  storageBucket: "VOTRE_STORAGE_BUCKET",
+  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
+  appId: "VOTRE_APP_ID"
+};
+
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporter les services Firebase pour les utiliser dans d'autres fichiers
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);

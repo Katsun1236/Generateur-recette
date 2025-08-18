4c5986e4bf6de817351706c050724a9b1655aa93
Katsun1246 <96658284+Katsun1236@users.noreply.github.com>
2025-08-18 15:56:36 +0000

    fix bug register

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,24 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// Importez les fonctions nécessaires depuis les SDK Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBZMnlHt0TzR33sRVaGJ9HZLLZVzEGnu-k",
-    authDomain: "ratio-faction.firebaseapp.com",
-    projectId: "ratio-faction",
-    storageBucket: "ratio-faction.firebasestorage.app",
-    messagingSenderId: "402552344518",
-    appId: "1:402552344518:web:cad898ebb7f701770cecc7",
-    measurementId: "G-20WL3ZM0XJ"
-  };
+// Configuration de votre application web Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBZMnlHt0TzR33sRVaGJ9HZLLZVzEGnu-k",
+  authDomain: "ratio-faction.firebaseapp.com",
+  projectId: "ratio-faction",
+  storageBucket: "ratio-faction.appspot.com", // J'ai corrigé une petite erreur ici, il manquait .appspot
+  messagingSenderId: "402552344518",
+  appId: "1:402552344518:web:cad898ebb7f701770cecc7",
+  measurementId: "G-20WL3ZM0XJ"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Initialiser Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporter les services Firebase pour les utiliser dans d'autres fichiers
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);

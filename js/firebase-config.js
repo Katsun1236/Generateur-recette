b81474530e2dcd7bacfb9198190e595ccbb804c5
Katsun1246 <96658284+Katsun1236@users.noreply.github.com>
2025-08-19 19:44:54 +0000

    fix3

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,24 +1,25 @@
-// Importez les fonctions nécessaires depuis les SDK Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+// Importe les fonctions nécessaires du SDK Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
 
-// Configuration de votre application web Firebase
+// Votre configuration d'application web Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyBZMnlHt0TzR33sRVaGJ9HZLLZVzEGnu-k",
   authDomain: "ratio-faction.firebaseapp.com",
   projectId: "ratio-faction",
-  storageBucket: "ratio-faction.appspot.com", // J'ai corrigé une petite erreur ici, il manquait .appspot
+  storageBucket: "ratio-faction.appspot.com", // J'ai corrigé une petite erreur ici, c'est généralement .appspot.com
   messagingSenderId: "402552344518",
   appId: "1:402552344518:web:cad898ebb7f701770cecc7",
   measurementId: "G-20WL3ZM0XJ"
 };
 
-// Initialiser Firebase
+// Initialise l'application Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporter les services Firebase pour les utiliser dans d'autres fichiers
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+// Initialise les services Firestore et Auth
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Exporte les services pour les utiliser dans d'autres fichiers
+export { db, auth };
